ac8918260443e09885d4999dcab7bb7218690f68
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2024-08-26 23:14:59 +0200

    Todo form/item/timer components and project page

---
+++ src/app/components/TodoTimer.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import React, { forwardRef, useImperativeHandle } from 'react';
+
+type TodoTimerRef =
+	| HTMLDivElement
+	| {
+			onClickStart: () => void;
+			onClickPause: () => void;
+			onClickStop: () => void;
+	  };
+
+type TodoTimerProps = {
+	intervalRef: React.MutableRefObject<NodeJS.Timeout | null>;
+	getDeadTime: (seconds: number) => Date;
+	getTimeRemaining: (e: Date) => Record<string, number>;
+	getTimeRemainingToTimerString: (e: Date) => string;
+	timer: string;
+	setTimer: React.Dispatch<React.SetStateAction<string>>;
+	sec: number;
+	toggleDone: () => void;
+	todoTitle: string;
+};
+
+const TodoTimer = forwardRef<TodoTimerRef, TodoTimerProps>(
+	(
+		{
+			intervalRef,
+			getDeadTime,
+			getTimeRemaining,
+			getTimeRemainingToTimerString,
+			timer,
+			setTimer,
+			sec,
+			toggleDone,
+			todoTitle,
+		},
+		ref,
+	) => {
+		const getTimerStringToSeconds = (timeString: string) => {
+			const parts = timeString.split(':');
+			const hours = parseInt(parts[0], 10);
+			const minutes = parseInt(parts[1], 10);
+			const seconds = parseInt(parts[2], 10);
+			return hours * 3600 + minutes * 60 + seconds;
+		};
+
+		const startTimer = (e: Date) => {
+			const { total } = getTimeRemaining(e);
+			if (total >= 0) {
+				// update the timer
+				setTimer(getTimeRemainingToTimerString(e));
+			} else {
+				if (
+					confirm(
+						`Time is over! Would you like to mark TODO "${todoTitle}" as done?`,
+					)
+				) {
+					onClickStop();
+					toggleDone();
+				} else {
+					onClickStop();
+				}
+			}
+		};
+
+		const onClickStart = () => {
+			const timerDateTime = getDeadTime(
+				(timer === getTimeRemainingToTimerString(getDeadTime(sec))
+					? sec // if timer is not started yet (not un-paused), then use default amount of seconds
+					: getTimerStringToSeconds(timer)) - 1, // -1 because we want to see timer starts immediately after click on a button
+			);
+			setTimer(getTimeRemainingToTimerString(timerDateTime)); // if remove this line the updating of timer Variable will be after 1000ms or 1sec
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+			const id = setInterval(() => {
+				startTimer(timerDateTime);
+			}, 1000);
+			intervalRef.current = id;
+		};
+
+		const onClickPause = () => {
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+			intervalRef.current = null;
+		};
+
+		const onClickStop = () => {
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+			intervalRef.current = null;
+			setTimer(getTimeRemainingToTimerString(getDeadTime(sec)));
+		};
+
+		useImperativeHandle(ref, () => ({
+			onClickStart,
+			onClickPause,
+			onClickStop,
+		}));
+
+		return <h2 className="text-xl font-bold mt-5">{timer}</h2>;
+	},
+);
+
+export default TodoTimer;
--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,16 +1,107 @@
 'use client';
 
-import React from 'react';
+import React, { useReducer } from 'react';
 import Link from 'next/link';
+import '../../globals.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import TodoForm from '../../components/TodoForm';
+import TodoItem from '../../components/TodoItem';
+import { useLocalStorage } from '../../hooks';
+import { LS_KEY_PROJECTS, LS_KEY_TODOS } from '../../consts';
+import type { Project, Todo } from '../../types';
 
-const Page = ({ params }: { params: { id: string } }) => {
+type TodosProps = {
+	params: { id: string };
+};
+
+const Todos: React.FC<TodosProps> = ({ params }) => {
+	const [, forceUpdate] = useReducer(x => x + 1, 0);
+
+	const projectId = params.id;
+
+	const [getLsTodos, setLsTodos] = useLocalStorage<Todo[]>(
+		LS_KEY_TODOS,
+		forceUpdate,
+	);
+	const [getLsProjects] = useLocalStorage<Project[]>(
+		LS_KEY_PROJECTS,
+		forceUpdate,
+	);
+	const selectedProject = getLsProjects().filter(p => p.id === projectId)[0];
+
+	const addTodo = (todo: Todo) => {
+		setLsTodos([...getLsTodos(), todo]);
+	};
+
+	const updateTodo = (updatedTodo: Todo) => {
+		setLsTodos(
+			getLsTodos().map(todo =>
+				todo.id === updatedTodo.id ? updatedTodo : todo,
+			),
+		);
+	};
+
+	const removeTodo = (id: string) => {
+		setLsTodos(getLsTodos().filter(todo => todo.id !== id));
+	};
+
 	return (
-		<div>
-			PROJECT ID: {params.id}
-			<br />
-			<Link href="/">Go back to homepage</Link>
-		</div>
+		<>
+			{projectId && (
+				<>
+					<h1 className="text-2xl font-bold underline mb-5">
+						<Link href="/">🗂️</Link>
+					</h1>
+					<h1 className="text-2xl font-bold mb-5">
+						{selectedProject?.title}
+					</h1>
+					{selectedProject?.description && (
+						<p className="mb-5">{selectedProject.description}</p>
+					)}
+					<TodoForm addTodo={addTodo} projectId={projectId} />
+					<ul className="w-full flex flex-col items-center">
+						{getLsTodos()
+							.filter(t => t.projectId === projectId && !t.isDone)
+							.sort(
+								(a, b) =>
+									(b.creationTimestamp || 0) -
+									(a.creationTimestamp || 0),
+							)
+							.map(todo => (
+								<TodoItem
+									key={todo.id}
+									todo={todo}
+									updateTodo={updateTodo}
+									removeTodo={removeTodo}
+								/>
+							))}
+					</ul>
+					{getLsTodos().filter(
+						t => t.projectId === projectId && t.isDone,
+					).length > 0 && (
+						<h1 className="text-2xl font-bold mb-5">DONE</h1>
+					)}
+					<ul className="w-full flex flex-col items-center">
+						{getLsTodos()
+							.filter(t => t.projectId === projectId && t.isDone)
+							.sort(
+								(a, b) =>
+									(b.doneTimestamp || 0) -
+									(a.doneTimestamp || 0),
+							)
+							.map(todo => (
+								<TodoItem
+									key={todo.id}
+									todo={todo}
+									updateTodo={updateTodo}
+									removeTodo={removeTodo}
+								/>
+							))}
+					</ul>
+				</>
+			)}
+		</>
 	);
 };
 
-export default Page;
+export default Todos;
