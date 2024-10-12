44ef5a8306f2f38a82f4ddfaa2f761c532036975
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2024-10-12 10:49:18 +0200

    Shadcn integrated in projects list and forms

--- src/components/ProjectForm.tsx
+++ src/components/ProjectForm.tsx
@@ -1,51 +1,75 @@
 'use client';
 
-import React, { ChangeEvent } from 'react';
-import Button from './Button';
-import InputText from './InputText';
+import React, { ChangeEvent, FormEvent } from 'react';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+import { Button } from './ui/button';
+import { Textarea } from './ui/textarea';
+import { CardContent, CardFooter, CardHeader, CardTitle } from './ui/card';
 
 type ProjectFormProps = {
+	header: string;
 	title: string;
 	setTitle: (value: React.SetStateAction<string>) => void;
 	description: string;
 	setDescription: (value: React.SetStateAction<string>) => void;
 	handleCancel: () => void;
+	handleSubmit: (e: FormEvent) => void;
 };
 
 const ProjectForm: React.FC<ProjectFormProps> = ({
+	header,
 	title,
 	setTitle,
 	description,
 	setDescription,
 	handleCancel,
+	handleSubmit,
 }) => (
-	<>
-		<InputText
-			type="text"
-			placeholder="Title"
-			value={title}
-			onChange={(e: ChangeEvent<HTMLInputElement>) =>
-				setTitle(e.target.value)
-			}
-			required
-		/>
-		<br />
-		<InputText
-			as="textarea"
-			className="block mt-5"
-			placeholder="Description"
-			value={description}
-			onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-				setDescription(e.target.value)
-			}
-		/>
-		<div className="block mt-5 text-right">
-			<Button className="mr-5" type="submit">
+	<form onSubmit={handleSubmit}>
+		<CardHeader>
+			<CardTitle>{header}</CardTitle>
+		</CardHeader>
+		<CardContent className="grid gap-6">
+			<div className="grid gap-2">
+				<Label htmlFor="title">Title</Label>
+				<Input
+					id="title"
+					placeholder="Project title..."
+					value={title}
+					onChange={(e: ChangeEvent<HTMLInputElement>) =>
+						setTitle(e.target.value)
+					}
+					required
+				/>
+			</div>
+			<div className="grid gap-2">
+				<Label htmlFor="description">Description</Label>
+				<Textarea
+					id="description"
+					placeholder="Please include all information relevant to your project..."
+					value={description}
+					onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+						setDescription(e.target.value)
+					}
+				/>
+			</div>
+		</CardContent>
+		<CardFooter className="justify-between space-x-2">
+			<Button
+				variant="ghost"
+				onClick={e => {
+					e.preventDefault();
+					handleCancel();
+				}}
+			>
+				Cancel
+			</Button>
+			<Button variant="outline" type="submit">
 				Save
 			</Button>
-			<Button onClick={handleCancel}>Cancel</Button>
-		</div>
-	</>
+		</CardFooter>
+	</form>
 );
 
 export default ProjectForm;
