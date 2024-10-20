6573e203c163d3d02f49cf2da50f7d43d87cfeb7
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2024-10-20 23:59:14 +0200

    Mobile top navigation header fixes

---
+++ src/components/SiteHeaderIcons.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import { Github, LogOut } from 'lucide-react';
+import { siteConfig } from '../config';
+import { useDispatchLogin, useLogin } from '../hooks';
+import { Button } from './ui/button';
+import ThemeToggle from './ThemeToggle';
+import MyAvatar from './MyAvatar';
+import { LOGIN_ACTIONS } from '../types';
+
+const SiteHeaderIcons: React.FC = () => {
+	const login = useLogin();
+	const dispatchLogin = useDispatchLogin();
+
+	return (
+		<nav className="flex items-center space-x-1">
+			<Link
+				href={siteConfig.links.github}
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Button size="icon" variant="ghost">
+					<Github className="h-5 w-5" />
+					<span className="sr-only">GitHub</span>
+				</Button>
+			</Link>
+			<ThemeToggle />
+			<Button
+				variant="ghost"
+				size="icon"
+				onClick={() =>
+					dispatchLogin({
+						action: LOGIN_ACTIONS.LOGOUT,
+						userId: '',
+						userName: '',
+					})
+				}
+				title={`Logout ${login.userName} (${login.userId})`}
+			>
+				<LogOut className="size-5" />
+				<span className="sr-only">Log out</span>
+			</Button>
+			<MyAvatar name={login.userName} />
+		</nav>
+	);
+};
+
+export default SiteHeaderIcons;
