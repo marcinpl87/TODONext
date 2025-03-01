8267f7899ec541d0d05621e9bafabb027fb15f0a
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2025-03-01 23:01:18 +0100

    Additional fields in tenant creation and editing forms

--- src/components/TenantCreate.tsx
+++ src/components/TenantCreate.tsx
@@ -15,6 +15,45 @@ const TenantCreate: React.FC<TenantCreateProps> = ({ addTenant }) => {
 	const [isOpened, setIsOpened] = useState<boolean>(false);
 	const [name, setName] = useState<string>('');
 	const [address, setAddress] = useState<string>('');
+	const [senderName, setSenderName] = useState<string>('');
+	const [idNumber, setIdNumber] = useState<string>('');
+	const [nationalInsuranceNumber, setNationalInsuranceNumber] =
+		useState<string>('');
+	const [birthDate, setBirthDate] = useState<string>('');
+	const [email, setEmail] = useState<string>('');
+	const [phone, setPhone] = useState<string>('');
+	const [rent, setRent] = useState<number>(0);
+	const [rentFirstMonth, setRentFirstMonth] = useState<string>('');
+	const [rentFirstRent, setRentFirstRent] = useState<number>(0);
+	const [deposit, setDeposit] = useState<number>(0);
+	const [account, setAccount] = useState<string>('');
+	const [accountDeposit, setAccountDeposit] = useState<string>('');
+	const [iceName, setIceName] = useState<string>('');
+	const [iceLastname, setIceLastname] = useState<string>('');
+	const [icePhone, setIcePhone] = useState<string>('');
+	const [iceEmail, setIceEmail] = useState<string>('');
+	const [iceIdNumber, setIceIdNumber] = useState<string>('');
+	const [iceNationalInsuranceNumber, setIceNationalInsuranceNumber] =
+		useState<string>('');
+	const [iceAddress, setIceAddress] = useState<string>('');
+	const [insuranceName, setInsuranceName] = useState<string>('');
+	const [insuranceDate, setInsuranceDate] = useState<string>('');
+	const [insuranceNumber, setInsuranceNumber] = useState<string>('');
+	const [notes, setNotes] = useState<string>('');
+	const [contractDate, setContractDate] = useState<string>('');
+	const [contractDateStart, setContractDateStart] = useState<string>('');
+	const [contractDateEnd, setContractDateEnd] = useState<string>('');
+	const [contractDateHandoff, setContractDateHandoff] = useState<string>('');
+	const [isContract, setIsContract] = useState<boolean>(false);
+	const [isDeposit, setIsDeposit] = useState<boolean>(false);
+	const [is1stRent, setIs1stRent] = useState<boolean>(false);
+	const [isInsurance, setIsInsurance] = useState<boolean>(false);
+	const [isKey, setIsKey] = useState<boolean>(false);
+	const [isWarranty, setIsWarranty] = useState<boolean>(false);
+	const [isProtocol, setIsProtocol] = useState<boolean>(false);
+	const [status, setStatus] = useState<number>(0);
+	const [roomId, setRoomId] = useState<string>('');
+	const [apartmentId, setApartmentId] = useState<string>('');
 
 	const handleSubmit = (e: FormEvent) => {
 		e.preventDefault();
@@ -24,47 +63,82 @@ const TenantCreate: React.FC<TenantCreateProps> = ({ addTenant }) => {
 				name,
 				address,
 				creationTimestamp: Date.now(),
-				senderName: '',
-				idNumber: '',
-				nationalInsuranceNumber: '',
-				birthDate: '',
-				email: '',
-				phone: '',
+				senderName,
+				idNumber,
+				nationalInsuranceNumber,
+				birthDate,
+				email,
+				phone,
 				roomId: uuidv4(),
 				apartmentId: uuidv4(),
-				rent: 0,
-				rentFirstMonth: '',
-				rentFirstRent: 0,
-				deposit: 0,
-				account: '',
-				accountDeposit: '',
-				iceName: '',
-				iceLastname: '',
-				icePhone: '',
-				iceEmail: '',
-				iceIdNumber: '',
-				iceNationalInsuranceNumber: '',
-				iceAddress: '',
-				insuranceName: '',
-				insuranceDate: '',
-				insuranceNumber: '',
-				notes: '',
-				contractDate: '',
-				contractDateStart: '',
-				contractDateEnd: '',
-				contractDateHandoff: '',
-				isContract: false,
-				isDeposit: false,
-				is1stRent: false,
-				isInsurance: false,
-				isKey: false,
-				isWarranty: false,
-				isProtocol: false,
-				status: 0,
+				rent,
+				rentFirstMonth,
+				rentFirstRent,
+				deposit,
+				account,
+				accountDeposit,
+				iceName,
+				iceLastname,
+				icePhone,
+				iceEmail,
+				iceIdNumber,
+				iceNationalInsuranceNumber,
+				iceAddress,
+				insuranceName,
+				insuranceDate,
+				insuranceNumber,
+				notes,
+				contractDate,
+				contractDateStart,
+				contractDateEnd,
+				contractDateHandoff,
+				isContract,
+				isDeposit,
+				is1stRent,
+				isInsurance,
+				isKey,
+				isWarranty,
+				isProtocol,
+				status,
 			},
 			() => {
 				setName('');
 				setAddress('');
+				setSenderName('');
+				setIdNumber('');
+				setNationalInsuranceNumber('');
+				setBirthDate('');
+				setEmail('');
+				setPhone('');
+				setRent(0);
+				setRentFirstMonth('');
+				setRentFirstRent(0);
+				setDeposit(0);
+				setAccount('');
+				setAccountDeposit('');
+				setIceName('');
+				setIceLastname('');
+				setIcePhone('');
+				setIceEmail('');
+				setIceIdNumber('');
+				setIceNationalInsuranceNumber('');
+				setIceAddress('');
+				setInsuranceName('');
+				setInsuranceDate('');
+				setInsuranceNumber('');
+				setNotes('');
+				setContractDate('');
+				setContractDateStart('');
+				setContractDateEnd('');
+				setContractDateHandoff('');
+				setIsContract(false);
+				setIsDeposit(false);
+				setIs1stRent(false);
+				setIsInsurance(false);
+				setIsKey(false);
+				setIsWarranty(false);
+				setIsProtocol(false);
+				setStatus(0);
 				setIsOpened(false);
 			},
 		);
@@ -84,6 +158,82 @@ const TenantCreate: React.FC<TenantCreateProps> = ({ addTenant }) => {
 						setName={setName}
 						address={address}
 						setAddress={setAddress}
+						senderName={senderName}
+						setSenderName={setSenderName}
+						idNumber={idNumber}
+						setIdNumber={setIdNumber}
+						nationalInsuranceNumber={nationalInsuranceNumber}
+						setNationalInsuranceNumber={setNationalInsuranceNumber}
+						birthDate={birthDate}
+						setBirthDate={setBirthDate}
+						email={email}
+						setEmail={setEmail}
+						phone={phone}
+						setPhone={setPhone}
+						rent={rent}
+						setRent={setRent}
+						rentFirstMonth={rentFirstMonth}
+						setRentFirstMonth={setRentFirstMonth}
+						rentFirstRent={rentFirstRent}
+						setRentFirstRent={setRentFirstRent}
+						deposit={deposit}
+						setDeposit={setDeposit}
+						account={account}
+						setAccount={setAccount}
+						accountDeposit={accountDeposit}
+						setAccountDeposit={setAccountDeposit}
+						iceName={iceName}
+						setIceName={setIceName}
+						iceLastname={iceLastname}
+						setIceLastname={setIceLastname}
+						icePhone={icePhone}
+						setIcePhone={setIcePhone}
+						iceEmail={iceEmail}
+						setIceEmail={setIceEmail}
+						iceIdNumber={iceIdNumber}
+						setIceIdNumber={setIceIdNumber}
+						iceNationalInsuranceNumber={iceNationalInsuranceNumber}
+						setIceNationalInsuranceNumber={
+							setIceNationalInsuranceNumber
+						}
+						iceAddress={iceAddress}
+						setIceAddress={setIceAddress}
+						insuranceName={insuranceName}
+						setInsuranceName={setInsuranceName}
+						insuranceDate={insuranceDate}
+						setInsuranceDate={setInsuranceDate}
+						insuranceNumber={insuranceNumber}
+						setInsuranceNumber={setInsuranceNumber}
+						notes={notes}
+						setNotes={setNotes}
+						contractDate={contractDate}
+						setContractDate={setContractDate}
+						contractDateStart={contractDateStart}
+						setContractDateStart={setContractDateStart}
+						contractDateEnd={contractDateEnd}
+						setContractDateEnd={setContractDateEnd}
+						contractDateHandoff={contractDateHandoff}
+						setContractDateHandoff={setContractDateHandoff}
+						isContract={isContract}
+						setIsContract={setIsContract}
+						isDeposit={isDeposit}
+						setIsDeposit={setIsDeposit}
+						is1stRent={is1stRent}
+						setIs1stRent={setIs1stRent}
+						isInsurance={isInsurance}
+						setIsInsurance={setIsInsurance}
+						isKey={isKey}
+						setIsKey={setIsKey}
+						isWarranty={isWarranty}
+						setIsProtocol={setIsProtocol}
+						isProtocol={isProtocol}
+						setIsWarranty={setIsWarranty}
+						status={status}
+						setStatus={setStatus}
+						roomId={roomId}
+						setRoomId={setRoomId}
+						apartmentId={apartmentId}
+						setApartmentId={setApartmentId}
 						handleCancel={handleCancel}
 						handleSubmit={handleSubmit}
 					/>
--- src/components/TenantItem.tsx
+++ src/components/TenantItem.tsx
@@ -22,6 +22,65 @@ const TenantItem: React.FC<TenantItemProps> = ({
 	const [isEditing, setIsEditing] = useState<boolean>(false);
 	const [name, setName] = useState<string>(tenant.name);
 	const [address, setAddress] = useState<string>(tenant.address);
+	const [senderName, setSenderName] = useState<string>(tenant.senderName);
+	const [idNumber, setIdNumber] = useState<string>(tenant.idNumber);
+	const [nationalInsuranceNumber, setNationalInsuranceNumber] =
+		useState<string>(tenant.nationalInsuranceNumber);
+	const [birthDate, setBirthDate] = useState<string>(tenant.birthDate);
+	const [email, setEmail] = useState<string>(tenant.email);
+	const [phone, setPhone] = useState<string>(tenant.phone);
+	const [roomId, setRoomId] = useState<string>(tenant.roomId);
+	const [apartmentId, setApartmentId] = useState<string>(tenant.apartmentId);
+	const [rent, setRent] = useState<number>(tenant.rent);
+	const [rentFirstMonth, setRentFirstMonth] = useState<string>(
+		tenant.rentFirstMonth,
+	);
+	const [rentFirstRent, setRentFirstRent] = useState<number>(
+		tenant.rentFirstRent,
+	);
+	const [deposit, setDeposit] = useState<number>(tenant.deposit);
+	const [account, setAccount] = useState<string>(tenant.account);
+	const [accountDeposit, setAccountDeposit] = useState<string>(
+		tenant.accountDeposit,
+	);
+	const [iceName, setIceName] = useState<string>(tenant.iceName);
+	const [iceLastname, setIceLastname] = useState<string>(tenant.iceLastname);
+	const [icePhone, setIcePhone] = useState<string>(tenant.icePhone);
+	const [iceEmail, setIceEmail] = useState<string>(tenant.iceEmail);
+	const [iceIdNumber, setIceIdNumber] = useState<string>(tenant.iceIdNumber);
+	const [iceNationalInsuranceNumber, setIceNationalInsuranceNumber] =
+		useState<string>(tenant.iceNationalInsuranceNumber);
+	const [iceAddress, setIceAddress] = useState<string>(tenant.iceAddress);
+	const [insuranceName, setInsuranceName] = useState<string>(
+		tenant.insuranceName,
+	);
+	const [insuranceDate, setInsuranceDate] = useState<string>(
+		tenant.insuranceDate,
+	);
+	const [insuranceNumber, setInsuranceNumber] = useState<string>(
+		tenant.insuranceNumber,
+	);
+	const [notes, setNotes] = useState<string>(tenant.notes);
+	const [contractDate, setContractDate] = useState<string>(
+		tenant.contractDate,
+	);
+	const [contractDateStart, setContractDateStart] = useState<string>(
+		tenant.contractDateStart,
+	);
+	const [contractDateEnd, setContractDateEnd] = useState<string>(
+		tenant.contractDateEnd,
+	);
+	const [contractDateHandoff, setContractDateHandoff] = useState<string>(
+		tenant.contractDateHandoff,
+	);
+	const [isContract, setIsContract] = useState<boolean>(tenant.isContract);
+	const [isDeposit, setIsDeposit] = useState<boolean>(tenant.isDeposit);
+	const [is1stRent, setIs1stRent] = useState<boolean>(tenant.is1stRent);
+	const [isInsurance, setIsInsurance] = useState<boolean>(tenant.isInsurance);
+	const [isKey, setIsKey] = useState<boolean>(tenant.isKey);
+	const [isWarranty, setIsWarranty] = useState<boolean>(tenant.isWarranty);
+	const [isProtocol, setIsProtocol] = useState<boolean>(tenant.isProtocol);
+	const [status, setStatus] = useState<number>(tenant.status);
 
 	const handleEdit = () => {
 		setIsEditing(true);
@@ -34,6 +93,43 @@ const TenantItem: React.FC<TenantItemProps> = ({
 				...tenant,
 				name,
 				address,
+				senderName,
+				idNumber,
+				nationalInsuranceNumber,
+				birthDate,
+				email,
+				phone,
+				roomId,
+				apartmentId,
+				rent,
+				rentFirstMonth,
+				rentFirstRent,
+				deposit,
+				account,
+				accountDeposit,
+				iceName,
+				iceLastname,
+				icePhone,
+				iceEmail,
+				iceIdNumber,
+				iceNationalInsuranceNumber,
+				iceAddress,
+				insuranceName,
+				insuranceDate,
+				insuranceNumber,
+				notes,
+				contractDate,
+				contractDateStart,
+				contractDateEnd,
+				contractDateHandoff,
+				isContract,
+				isDeposit,
+				is1stRent,
+				isInsurance,
+				isKey,
+				isWarranty,
+				isProtocol,
+				status,
 			},
 			() => {
 				setIsEditing(false);
@@ -58,6 +154,82 @@ const TenantItem: React.FC<TenantItemProps> = ({
 					setName={setName}
 					address={address}
 					setAddress={setAddress}
+					senderName={senderName}
+					setSenderName={setSenderName}
+					idNumber={idNumber}
+					setIdNumber={setIdNumber}
+					nationalInsuranceNumber={nationalInsuranceNumber}
+					setNationalInsuranceNumber={setNationalInsuranceNumber}
+					birthDate={birthDate}
+					setBirthDate={setBirthDate}
+					email={email}
+					setEmail={setEmail}
+					phone={phone}
+					setPhone={setPhone}
+					roomId={roomId}
+					setRoomId={setRoomId}
+					apartmentId={apartmentId}
+					setApartmentId={setApartmentId}
+					rent={rent}
+					setRent={setRent}
+					rentFirstMonth={rentFirstMonth}
+					setRentFirstMonth={setRentFirstMonth}
+					rentFirstRent={rentFirstRent}
+					setRentFirstRent={setRentFirstRent}
+					deposit={deposit}
+					setDeposit={setDeposit}
+					account={account}
+					setAccount={setAccount}
+					accountDeposit={accountDeposit}
+					setAccountDeposit={setAccountDeposit}
+					iceName={iceName}
+					setIceName={setIceName}
+					iceLastname={iceLastname}
+					setIceLastname={setIceLastname}
+					icePhone={icePhone}
+					setIcePhone={setIcePhone}
+					iceEmail={iceEmail}
+					setIceEmail={setIceEmail}
+					iceIdNumber={iceIdNumber}
+					setIceIdNumber={setIceIdNumber}
+					iceNationalInsuranceNumber={iceNationalInsuranceNumber}
+					setIceNationalInsuranceNumber={
+						setIceNationalInsuranceNumber
+					}
+					iceAddress={iceAddress}
+					setIceAddress={setIceAddress}
+					insuranceName={insuranceName}
+					setInsuranceName={setInsuranceName}
+					insuranceDate={insuranceDate}
+					setInsuranceDate={setInsuranceDate}
+					insuranceNumber={insuranceNumber}
+					setInsuranceNumber={setInsuranceNumber}
+					notes={notes}
+					setNotes={setNotes}
+					contractDate={contractDate}
+					setContractDate={setContractDate}
+					contractDateStart={contractDateStart}
+					setContractDateStart={setContractDateStart}
+					contractDateEnd={contractDateEnd}
+					setContractDateEnd={setContractDateEnd}
+					contractDateHandoff={contractDateHandoff}
+					setContractDateHandoff={setContractDateHandoff}
+					isContract={isContract}
+					setIsContract={setIsContract}
+					isDeposit={isDeposit}
+					setIsDeposit={setIsDeposit}
+					is1stRent={is1stRent}
+					setIs1stRent={setIs1stRent}
+					isInsurance={isInsurance}
+					setIsInsurance={setIsInsurance}
+					isKey={isKey}
+					setIsKey={setIsKey}
+					isWarranty={isWarranty}
+					setIsWarranty={setIsWarranty}
+					isProtocol={isProtocol}
+					setIsProtocol={setIsProtocol}
+					status={status}
+					setStatus={setStatus}
 					handleCancel={handleCancel}
 					handleSubmit={handleSubmit}
 				/>
