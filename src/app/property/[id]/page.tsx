eb882b37800cffcc83c5d6531d0bf7482b603a42
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2025-03-08 21:34:59 +0100

    Columns layout on property and tenant details pages

--- src/app/property/[id]/page.tsx
+++ src/app/property/[id]/page.tsx
@@ -40,48 +40,58 @@ const PropertyDetails: React.FC<PropertyDetailsProps> = ({ params }) => {
 	return (
 		<div className="flex flex-col items-center max-w-4xl m-auto">
 			<h1 className="text-2xl font-bold my-5">{property.name}</h1>
-			<Card className="w-full">
-				<p>
-					<strong>Address:</strong> {property.address}
-				</p>
-				<p>
-					<strong>Floor:</strong> {property.floor}
-				</p>
-				<p>
-					<strong>Code:</strong> {property.code}
-				</p>
-				<p>
-					<strong>WiFi SSID:</strong> {property.wifiSsid}
-				</p>
-				<p>
-					<strong>WiFi Password:</strong> {property.wifiPass}
-				</p>
-				<p>
-					<strong>Rooms:</strong> {property.rooms}
-				</p>
-				<p>
-					<strong>Lock In:</strong> {property.lockIn}
-				</p>
-				<p>
-					<strong>Lock Out:</strong> {property.lockOut}
-				</p>
-				<p>
-					<strong>Safe:</strong> {property.safe}
-				</p>
-				<p>
-					<strong>Insurance Name:</strong> {property.insuranceName}
-				</p>
-				<p>
-					<strong>Insurance Date:</strong> {property.insuranceDate}
-				</p>
-				<p>
-					<strong>Insurance Number:</strong>{' '}
-					{property.insuranceNumber}
-				</p>
-				<p>
-					<strong>Notes:</strong> {property.notes}
-				</p>
-			</Card>
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+				<Card className="p-4">
+					<h2 className="text-xl font-bold mb-3">
+						Property Information
+					</h2>
+					<p>
+						<strong>Address:</strong> {property.address}
+					</p>
+					<p>
+						<strong>Floor:</strong> {property.floor}
+					</p>
+					<p>
+						<strong>Code:</strong> {property.code}
+					</p>
+					<p>
+						<strong>WiFi SSID:</strong> {property.wifiSsid}
+					</p>
+					<p>
+						<strong>WiFi Password:</strong> {property.wifiPass}
+					</p>
+					<p>
+						<strong>Rooms:</strong> {property.rooms}
+					</p>
+					<p>
+						<strong>Lock In:</strong> {property.lockIn}
+					</p>
+					<p>
+						<strong>Lock Out:</strong> {property.lockOut}
+					</p>
+					<p>
+						<strong>Safe:</strong> {property.safe}
+					</p>
+					<p>
+						<strong>Insurance Name:</strong>{' '}
+						{property.insuranceName}
+					</p>
+					<p>
+						<strong>Insurance Date:</strong>{' '}
+						{property.insuranceDate}
+					</p>
+					<p>
+						<strong>Insurance Number:</strong>{' '}
+						{property.insuranceNumber}
+					</p>
+				</Card>
+				<Card className="p-4">
+					<h2 className="text-xl font-bold mb-3">Notes</h2>
+					<p>
+						<strong>Notes:</strong> {property.notes}
+					</p>
+				</Card>
+			</div>
 		</div>
 	);
 };
--- src/app/tenant/[id]/page.tsx
+++ src/app/tenant/[id]/page.tsx
@@ -40,132 +40,158 @@ const TenantDetails: React.FC<TenantDetailsProps> = ({ params }) => {
 	return (
 		<div className="flex flex-col items-center max-w-4xl m-auto">
 			<h1 className="text-2xl font-bold my-5">{tenant.name}</h1>
-			<Card className="w-full">
-				<p>
-					<strong>Address:</strong> {tenant.address}
-				</p>
-				<p>
-					<strong>Sender Name:</strong> {tenant.senderName}
-				</p>
-				<p>
-					<strong>ID Number:</strong> {tenant.idNumber}
-				</p>
-				<p>
-					<strong>National Insurance Number:</strong>{' '}
-					{tenant.nationalInsuranceNumber}
-				</p>
-				<p>
-					<strong>Birth Date:</strong> {tenant.birthDate}
-				</p>
-				<p>
-					<strong>Email:</strong> {tenant.email}
-				</p>
-				<p>
-					<strong>Phone:</strong> {tenant.phone}
-				</p>
-				<p>
-					<strong>Room ID:</strong> {tenant.roomId}
-				</p>
-				<p>
-					<strong>Apartment ID:</strong> {tenant.apartmentId}
-				</p>
-				<p>
-					<strong>Rent:</strong> {tenant.rent}
-				</p>
-				<p>
-					<strong>Rent First Month:</strong> {tenant.rentFirstMonth}
-				</p>
-				<p>
-					<strong>Rent First Rent:</strong> {tenant.rentFirstRent}
-				</p>
-				<p>
-					<strong>Deposit:</strong> {tenant.deposit}
-				</p>
-				<p>
-					<strong>Account:</strong> {tenant.account}
-				</p>
-				<p>
-					<strong>Account Deposit:</strong> {tenant.accountDeposit}
-				</p>
-				<p>
-					<strong>ICE Name:</strong> {tenant.iceName}
-				</p>
-				<p>
-					<strong>ICE Lastname:</strong> {tenant.iceLastname}
-				</p>
-				<p>
-					<strong>ICE Phone:</strong> {tenant.icePhone}
-				</p>
-				<p>
-					<strong>ICE Email:</strong> {tenant.iceEmail}
-				</p>
-				<p>
-					<strong>ICE ID Number:</strong> {tenant.iceIdNumber}
-				</p>
-				<p>
-					<strong>ICE National Insurance Number:</strong>{' '}
-					{tenant.iceNationalInsuranceNumber}
-				</p>
-				<p>
-					<strong>ICE Address:</strong> {tenant.iceAddress}
-				</p>
-				<p>
-					<strong>Insurance Name:</strong> {tenant.insuranceName}
-				</p>
-				<p>
-					<strong>Insurance Date:</strong> {tenant.insuranceDate}
-				</p>
-				<p>
-					<strong>Insurance Number:</strong> {tenant.insuranceNumber}
-				</p>
-				<p>
-					<strong>Notes:</strong> {tenant.notes}
-				</p>
-				<p>
-					<strong>Contract Date:</strong> {tenant.contractDate}
-				</p>
-				<p>
-					<strong>Contract Date Start:</strong>{' '}
-					{tenant.contractDateStart}
-				</p>
-				<p>
-					<strong>Contract Date End:</strong> {tenant.contractDateEnd}
-				</p>
-				<p>
-					<strong>Contract Date Handoff:</strong>{' '}
-					{tenant.contractDateHandoff}
-				</p>
-				<p>
-					<strong>Is Contract:</strong>{' '}
-					{tenant.isContract ? 'Yes' : 'No'}
-				</p>
-				<p>
-					<strong>Is Deposit:</strong>{' '}
-					{tenant.isDeposit ? 'Yes' : 'No'}
-				</p>
-				<p>
-					<strong>Is 1st Rent:</strong>{' '}
-					{tenant.is1stRent ? 'Yes' : 'No'}
-				</p>
-				<p>
-					<strong>Is Insurance:</strong>{' '}
-					{tenant.isInsurance ? 'Yes' : 'No'}
-				</p>
-				<p>
-					<strong>Is Key:</strong> {tenant.isKey ? 'Yes' : 'No'}
-				</p>
-				<p>
-					<strong>Is Warranty:</strong>{' '}
-					{tenant.isWarranty ? 'Yes' : 'No'}
-				</p>
-				<p>
-					<strong>Is Protocol:</strong>{' '}
-					{tenant.isProtocol ? 'Yes' : 'No'}
-				</p>
-				<p>
-					<strong>Status:</strong> {tenant.status}
-				</p>
-			</Card>
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
+				<Card className="p-4">
+					<h2 className="text-xl font-bold mb-3">
+						Tenant Information
+					</h2>
+					<p>
+						<strong>Address:</strong> {tenant.address}
+					</p>
+					<p>
+						<strong>Sender Name:</strong> {tenant.senderName}
+					</p>
+					<p>
+						<strong>ID Number:</strong> {tenant.idNumber}
+					</p>
+					<p>
+						<strong>National Insurance Number:</strong>{' '}
+						{tenant.nationalInsuranceNumber}
+					</p>
+					<p>
+						<strong>Birth Date:</strong> {tenant.birthDate}
+					</p>
+					<p>
+						<strong>Email:</strong> {tenant.email}
+					</p>
+					<p>
+						<strong>Phone:</strong> {tenant.phone}
+					</p>
+					<p>
+						<strong>Room ID:</strong> {tenant.roomId}
+					</p>
+					<p>
+						<strong>Apartment ID:</strong> {tenant.apartmentId}
+					</p>
+					<p>
+						<strong>Rent:</strong> {tenant.rent}
+					</p>
+					<p>
+						<strong>Rent First Month:</strong>{' '}
+						{tenant.rentFirstMonth}
+					</p>
+					<p>
+						<strong>Rent First Rent:</strong> {tenant.rentFirstRent}
+					</p>
+					<p>
+						<strong>Deposit:</strong> {tenant.deposit}
+					</p>
+					<p>
+						<strong>Account:</strong> {tenant.account}
+					</p>
+					<p>
+						<strong>Account Deposit:</strong>{' '}
+						{tenant.accountDeposit}
+					</p>
+					<p>
+						<strong>Notes:</strong> {tenant.notes}
+					</p>
+					<p>
+						<strong>Is Contract:</strong>{' '}
+						{tenant.isContract ? 'Yes' : 'No'}
+					</p>
+					<p>
+						<strong>Is Deposit:</strong>{' '}
+						{tenant.isDeposit ? 'Yes' : 'No'}
+					</p>
+					<p>
+						<strong>Is 1st Rent:</strong>{' '}
+						{tenant.is1stRent ? 'Yes' : 'No'}
+					</p>
+					<p>
+						<strong>Is Insurance:</strong>{' '}
+						{tenant.isInsurance ? 'Yes' : 'No'}
+					</p>
+					<p>
+						<strong>Is Key:</strong> {tenant.isKey ? 'Yes' : 'No'}
+					</p>
+					<p>
+						<strong>Is Warranty:</strong>{' '}
+						{tenant.isWarranty ? 'Yes' : 'No'}
+					</p>
+					<p>
+						<strong>Is Protocol:</strong>{' '}
+						{tenant.isProtocol ? 'Yes' : 'No'}
+					</p>
+					<p>
+						<strong>Status:</strong> {tenant.status}
+					</p>
+				</Card>
+				<div className="flex flex-col gap-6">
+					<Card className="p-4">
+						<h2 className="text-xl font-bold mb-3">
+							Emergency Contact
+						</h2>
+						<p>
+							<strong>Name:</strong> {tenant.iceName}
+						</p>
+						<p>
+							<strong>Lastname:</strong> {tenant.iceLastname}
+						</p>
+						<p>
+							<strong>Phone:</strong> {tenant.icePhone}
+						</p>
+						<p>
+							<strong>Email:</strong> {tenant.iceEmail}
+						</p>
+						<p>
+							<strong>ID Number:</strong> {tenant.iceIdNumber}
+						</p>
+						<p>
+							<strong>National Insurance Number:</strong>{' '}
+							{tenant.iceNationalInsuranceNumber}
+						</p>
+						<p>
+							<strong>Address:</strong> {tenant.iceAddress}
+						</p>
+					</Card>
+					<Card className="p-4">
+						<h2 className="text-xl font-bold mb-3">
+							Contract Dates
+						</h2>
+						<p>
+							<strong>Contract Date:</strong>{' '}
+							{tenant.contractDate}
+						</p>
+						<p>
+							<strong>Start Date:</strong>{' '}
+							{tenant.contractDateStart}
+						</p>
+						<p>
+							<strong>End Date:</strong> {tenant.contractDateEnd}
+						</p>
+						<p>
+							<strong>Handoff Date:</strong>{' '}
+							{tenant.contractDateHandoff}
+						</p>
+					</Card>
+					<Card className="p-4">
+						<h2 className="text-xl font-bold mb-3">Insurance</h2>
+						<p>
+							<strong>Insurance Name:</strong>{' '}
+							{tenant.insuranceName}
+						</p>
+						<p>
+							<strong>Insurance Date:</strong>{' '}
+							{tenant.insuranceDate}
+						</p>
+						<p>
+							<strong>Insurance Number:</strong>{' '}
+							{tenant.insuranceNumber}
+						</p>
+					</Card>
+				</div>
+			</div>
 		</div>
 	);
 };
